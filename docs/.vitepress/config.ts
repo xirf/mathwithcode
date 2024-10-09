6a564620e37c96b760776aa5d9ebc97560966ff0
xirf <andikanp@proton.me>
2024-10-10 00:27:31 +0700

    feat: linear-equation

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitepress'
 import { sharedConfig } from './shared.config'
+import sidebar from './sidebar'
 
 
 // https://vitepress.dev/reference/site-config
@@ -23,24 +24,12 @@ export default defineConfig({
             text: 'Donasi', link: 'https://trakteer.id/xirf'
           }
         ],
-        sidebar: [
-          { text: 'Panduan', link: '/guide/' },
-          {
-            text: 'Aritmatika',
-            collapsed: false,
-            items: [
-              { text: 'Bilangan', link: '/arithmetic/numbers' },
-              { text: 'Operasi Bilangan', link: '/arithmetic/operation-on-number' },
-              { text: 'Faktor dan Kelipatan', link: '/arithmetic/factors' },
-              { text: 'Persen', link: '/arithmetic/percent' },
-            ]
-          },
-        ],
+        sidebar,
         editLink: {
           pattern: 'https://github.com/xirf/mathwithcode/edit/master/docs/:path',
           text: 'Perbaiki halaman ini di GitHub'
         },
-
+        
         footer: {
           message: 'Dirilis di bawah lisensi MIT',
           copyright: 'Copyright © 2024-present Anka Tama & Contributors'
---
+++ docs/.vitepress/sidebar.ts
@@ -0,0 +1,24 @@
+export default [
+    { text: 'Panduan', link: '/guide/' },
+    {
+        text: 'Aritmatika',
+        collapsed: false,
+        items: [
+            { text: 'Bilangan', link: '/arithmetic/numbers' },
+            { text: 'Operasi Bilangan', link: '/arithmetic/operation-on-number' },
+            { text: 'Faktor dan Kelipatan', link: '/arithmetic/factors' },
+            { text: 'Persen', link: '/arithmetic/percent' },
+        ]
+    },
+    {
+        text: 'Aljabar',
+        collapsed: false,
+        items: [
+            { text: 'Persamaan Linear', link: '/algebra/linear-equation' },
+            { text: 'Persamaan Kuadrat', link: '/algebra/quadratic-equation' },
+            { text: 'Fungsi', link: '/algebra/function' },
+            { text: 'Grafik', link: '/algebra/graph' },
+            
+        ]
+    }
+]
