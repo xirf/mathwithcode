f07c16d7a00e362cbcf436eae096233264ecae35
xirf <andikanp@proton.me>
2024-10-13 14:55:24 +0700

    feat: umami analytics

--- docs/.vitepress/shared.config.ts
+++ docs/.vitepress/shared.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from "vitepress";
+import { defineConfig, type HeadConfig } from "vitepress";
 import { visualizer } from 'rollup-plugin-visualizer'
 import UnocssVitePlugin from 'unocss/vite'
 import { tabsMarkdownPlugin } from "vitepress-plugin-tabs"
+
+
+const umamiScript: HeadConfig = [ "script", {
+    defer: "true",
+    src: "https://cloud.umami.is/script.js",
+    "data-website-id": "f120cb10-6927-4e74-b8e7-49dd74e2f057"
+}]
+
+const baseHeader: HeadConfig[] = []
+
+const headers = process.env.NODE_ENV === "production" ?
+    baseHeader.concat(umamiScript) :
+    baseHeader
 
 export const sharedConfig = defineConfig({
     title: "Math W Code",
     description: "Belajar matematika dengan barisan kode",
     cleanUrls: true,
+    head: headers,
     metaChunk: true,
     lastUpdated: true,
     markdown: {
